8429cbb3eb1cce7b68e53a8f5fb085be7d877256
Nataliie <leychak@gmail.com>
2018-06-27 11:02:36 +0300

    edit_typecheck

--- functions.py
+++ functions.py
@@ -1,13 +1,15 @@
-from ExtList import *
+#from ExtList import *
 
-myList = ExtList(6)
-myList = myList.fillList()
-print 'Generated list:',myList
-#myList = [82.482, 'ag', 49.118, 48.289, 6, 'itpdet', 10, '10']
+#myList = ExtList(6)
+#myList = myList.fillList()
+#print 'Generated list:',myList
+myList = [82.482, 'ag', 49.118, 48.289, 6, 'itpdet', 10, '10']
 d = None
 VAT = 3.0
 aa = 100
 ba = 300
+
+#### Type check ---- if type(a) in (int, float)
 
 def int_or_float(inst):
     if isinstance(inst, int) or isinstance(inst, float):
@@ -22,7 +24,7 @@ def even_prnt(List):
         if type(i) == int:
             if i%2 == 0: print i
             
-even_prnt(myList)
+
 
 def calc(a, b):
     if (int_or_float(a) and int_or_float(b)) or (isinstance(a, str) and isinstance(b, str)):
